bd1e44f58a6ef745251bcd75cc224d033c78c32b
yunheejo <rhcjrdbsgml@gmail.com>
2022-09-12 22:25:37 +0900

    Redux count 생성

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { legacy_createStore } from "redux";
+
+const add = document.querySelector("#add");
+const minus = document.querySelector("#minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = legacy_createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMINUS = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMINUS);
+
+console.log(countStore.getState());
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
